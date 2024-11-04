0fabe67a79ab73759480adb51313624cd2fb2c02
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-04 13:05:14 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,89 @@
+
+function neop_schreibe_zch(zch_str: string = "A", color: number) {
+    let zeichen_matrix2: Array<number> = []
+
+    let mx2 = arr_tech_matrix[0];
+    let my2 = zch_str.length;
+    arr_tech_matrix[1] = my2 * 5;
+
+    // strip.showRainbow(1, 360)
+
+    let myColor: number = color;
+    let is_type2 = 0; //buchstabe
+
+    let zch_len2 = zch_str.length
+    if (zch_len2 > 1) {
+        is_type2 = 1; // wort
+        let arr_split2 = zch_str.split(",")
+        if (arr_split2.length > 2) {
+            is_type2 = 2; //array 
+            zeichen_matrix2 = arr_split2.map(wert => parseInt(wert));
+            zch_len2 = 1;
+        }
+    }
+
+    strip.clear()
+    strip.show()
+    let mycolor = randColor();
+
+    for (let o = 0; o < zch_len2; o++) {
+        myColor = randColor();
+        let zch2: string;
+        if (is_type2 < 2) { //no array
+            zch2 = zch_str[o]
+            if (zch2 == " ") {
+                myColor = randColor();
+            }
+            let found2 = bst_reihe.indexOf(zch2);
+            if (found2 == -1) {
+                found2 = 0;
+            }
+            zeichen_matrix2 = arr_zeichen[found2];
+        }
+        // strip.clear()
+        // strip.show()
+
+
+        // basic.showString(zch2)
+        zeichen_matrix2.forEach(function (zahl, zeile) {
+            for (let bit2 = 0; bit2 < mx2; bit2++) {
+                // let z = zeile, c = bit2 //7- minus
+                let c = zeile, z = bit2 //7- minus 2024
+                //b=z, z=bit
+                //b=z, z=mx-1-bit
+                if (zahl & Math.pow(2, (bit2 + shift + mx2) % mx2)) {
+                    // let px2 = z * mx2 + ((z % 2) ? (mx2 - 1 - c) : c)
+                    // let px2 = z * mx2 + ((z % 2) == 0 ? (mx2 - 1 - c) : c)
+                    let px2 = z * mx2 +c 
+
+                    strip.setPixelColor(px2, myColor);
+                    // strip.showRainbow(1, 360)
+                    //    basic.showNumber(c)
+                }
+            }
+        })
+        strip.show()
+        // arr_neop_strips[sss].show();
+        if (is_type2 == 1) {
+            pause(strip_pause)
+            // basic.showString(zch2)
+        }
+
+        strip.rotate(8 * 7);
+        strip.show();
+
+        // strip.rotate(0);
+        // strip.show();
+    }
+    automat = true;
+
+}
+
+function randColor() {
+    return [NeoPixelColors.Red, NeoPixelColors.Green, NeoPixelColors.Blue, NeoPixelColors.Yellow][Math.randomRange(0, 3)]
+}
+
+
 function init_alphabet() {
     bst_reihe = " ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜäöü0123456789!?.,*+-=≠:%abcdefghijklmnopqrstuvwxyz()<>";
     arr_zeichen = [
@@ -108,14 +194,14 @@ function loesche_matrix () {
 function init_strip () {
     pixelAnzahl = arr_tech_matrix[0] * arr_tech_matrix[1]
     strip = neopixel.create(0, pixelAnzahl, NeoPixelMode.RGB)
-strip.setBrightness(strip_helligkeit)
+    strip.setBrightness(strip_helligkeit)
     strip.clear()
 }
 let pixelAnzahl = 0
 let strip_helligkeit = 0
 let arr_tech_matrix: number[] = []
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 168, NeoPixelMode.RGB)
 // auch  scrollspeed
 let automat = false
 automat=true;
@@ -124,24 +210,34 @@ let shift: number = 0
 
 basic.showIcon(IconNames.SmallSquare)
 arr_tech_matrix = [8, 8]
-strip_helligkeit = 25
+strip_helligkeit = 50
 let strip_pause = 250
-strip.showColor(neopixel.colors(NeoPixelColors.Green))
-strip.show()
+// strip.showColor(neopixel.colors(NeoPixelColors.Green))
+// strip.show()
 // strip.rotate(8)
 // strip.show()
 let bst_reihe: string = "";
 let arr_zeichen: number[][];
+strip.setBrightness(strip_helligkeit);
 init_alphabet();
 
-neop_schreibe_zch("A", NeoPixelColors.Red)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Red))
+
+strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Red))
+
+
+neop_schreibe_zch("ABC", NeoPixelColors.Red)
 
 
 // strip.showRainbow(1, 360)
 
 basic.forever(() => {
     if (automat) {
-        strip.rotate(16 * 1);
+        strip.rotate(8);
         strip.show();
 
         // arr_neop_strips[1].rotate(16 * 1);
@@ -161,86 +257,3 @@ basic.forever(() => {
 
 
 
-
-function neop_schreibe_zch(zch_str: string = "A", color: number) {
-    let zeichen_matrix2: Array<number> = []
-
-    let mx2 = arr_tech_matrix[0];
-    let my2 = zch_str.length;
-    arr_tech_matrix[1] = my2 * 6;
-    
-    // strip.showRainbow(1, 360)
-
-    let myColor: number = color;
-    let is_type2 = 0; //buchstabe
-    
-    let zch_len2 = zch_str.length
-    if (zch_len2 > 1) {
-        is_type2 = 1; // wort
-        let arr_split2 = zch_str.split(",")
-        if (arr_split2.length > 2) {
-            is_type2 = 2; //array 
-            zeichen_matrix2 = arr_split2.map(wert => parseInt(wert));
-            zch_len2 = 1;
-        }
-    }
-
-    strip.clear()
-    strip.show()
-    let mycolor = randColor();
-
-    for (let o = 0; o < zch_len2; o++) {
-
-        // myColor = randColor();
-        let zch2: string;
-        if (is_type2 < 2) { //no array
-            zch2 = zch_str[o]
-            if (zch2 == " ") {
-                myColor = randColor();
-            }
-            let found2 = bst_reihe.indexOf(zch2);
-            if (found2 == -1) {
-                found2 = 0;
-            }
-            zeichen_matrix2 = arr_zeichen[found2];
-        }
-        // strip.clear()
-        // strip.show()
-
-
-        // basic.showString(zch2)
-        zeichen_matrix2.forEach(function (zahl, zeile) {
-            for (let bit2 = 0; bit2 < mx2; bit2++) {
-                // let z = zeile, c = bit2 //7- minus
-                let c = zeile, z = bit2 //7- minus 2024
-                //b=z, z=bit
-                //b=z, z=mx-1-bit
-                if (zahl & Math.pow(2, (bit2 + shift + mx2) % mx2)) {
-                    // let px2 = z * mx2 + ((z % 2) ? (mx2 - 1 - c) : c)
-                    let px2 = z * mx2 + ((z % 2) == 0 ? (mx2 - 1 - c) : c)
-                    strip.setPixelColor(px2, myColor);
-                    // strip.showRainbow(1, 360)
-                    //    basic.showNumber(c)
-                }
-            }
-        })
-        strip.show()
-        // arr_neop_strips[sss].show();
-        if (is_type2 == 1) {
-            pause(strip_pause)
-            // basic.showString(zch2)
-        }
-
-        // arr_neop_strips[sss].rotate(16 * 3);
-        // arr_neop_strips[sss].show();
-
-        // strip.rotate(0);
-        // strip.show();
-    }
-    automat = true;
-
-}
-
-function randColor() {
-    return [NeoPixelColors.Red, NeoPixelColors.Green, NeoPixelColors.Blue, NeoPixelColors.Yellow][Math.randomRange(0, 3)]
-}
